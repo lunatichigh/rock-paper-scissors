c7dc52d154a050040d29b533aea6561491904aaf
Zack M. <22827148+lunatichigh@users.noreply.github.com>
2024-10-25 11:44:01 -0400

    Add human choice logic

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+//Gets the computer's choice
 function getComputerChoice(min, max){
   return Math.random() * (max - min) + 0;
 }
@@ -9,9 +9,24 @@ console.log(computerChoice);
 if (computerChoice == 0){
     console.log("Rock");
 } else if (computerChoice == 1) {
-    console.log("Scissors");
-} else {
     console.log("Paper");
+} else {
+    console.log("Scissors");
 }
 
-// console.log(Math.round(getComputerChoice()));
+//Gets the human's choice
+let humanChoice = window.prompt("Pick Rock, Paper or Scissors:" ,);
+console.log(humanChoice);
+
+function getHumanChoice(){
+    let humanChoice = window.prompt("Pick Rock, Paper or Scissors:" ,);
+console.log(humanChoice);
+}
+
+if (humanChoice === "Rock"){
+    console.log("You chose Rock.");
+} else if (humanChoice === "Paper"){
+    console.log("You chose Paper.");
+} else {
+    console.log("You chose Scissors.");
+}
